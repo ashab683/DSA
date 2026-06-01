b596897d17300f62c7c9886e6e473f8dc568d71f
mohd ashab <mohdashab683@gmail.com>
2026-06-02 02:32:19 +0530

    Add files via upload

---
+++ 01_start/operators.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    //relation operators
+int a = 3;
+int b = 2;
+bool first = (a==b);
+cout<<first<<endl;
+
+bool second = (a>b);
+cout<<second<<endl;
+
+bool third = (a>=b);
+cout<<third<<endl;
+
+bool fourth = (a<b);
+cout<<fourth<<endl;
+
+bool fifth = (a<=b);
+cout<<fifth<<endl;
+
+bool sixth = (a!=b);
+cout<<sixth<<endl;
+
+
+int c = 21;
+cout<<!a<<endl;
+
+    return 0;
+}
