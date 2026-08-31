7075189d64e78f29a678c00991e0fb926b1c6a97
mohd ashab <mohdashab683@gmail.com>
2026-08-31 12:59:00 +0530

    practice array in pointer

--- 19_Pointer/pointer-array.cpp
+++ 19_Pointer/pointer-array.cpp
@@ -21,6 +21,20 @@ int arr[10] = {2,5,4,9};
 // cout<<sizeof(ptr)<<endl;
 // cout<<sizeof(*ptr)<<endl;
 
+int a[20] = {1,2,3,4,5};
+cout<<&a[0]<<endl;
+cout<<&a<<endl;
+cout<<a<<endl;
+
+int *p = &a[0];
+cout<<p<<endl;
+cout<<*p<<endl;
+cout<<&p<<endl;
+
+
+
+
+
 
 
 return 0;
